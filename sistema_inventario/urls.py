76fdb8829c95780892c93d6a3c07dce78ed81731
ASCiiLex <alejandroscuetara@gmail.com>
2026-03-14 13:30:47 +0100

    últimos ajustes post-integración

--- sistema_inventario/urls.py
+++ sistema_inventario/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +16,9 @@ urlpatterns = [
     path('categorias/', include('categories.urls')),
     path('proveedores/', include('suppliers.urls')),
     path('productos/', include('products.urls')),
+    path("movimientos/", lambda r: redirect("/stock-movements/")),
+    path("stock-movements/", lambda r: redirect("/inventory/stock-movements/")),
+
     
 
     # Notificaciones
