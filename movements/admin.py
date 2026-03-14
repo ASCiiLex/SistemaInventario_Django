2ff83c9b26764e43e30cb87eab85b639f6c0e244
ASCiiLex <alejandroscuetara@gmail.com>
2026-03-14 13:23:19 +0100

    Borrado de movements

--- movements/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Movement
-
-@admin.register(Movement)
-class MovementAdmin(admin.ModelAdmin):
-    list_display = ('product', 'movement_type', 'quantity', 'created_at')
-    list_filter = ('movement_type', 'created_at')
-    search_fields = ('product__name', 'product__sku')
