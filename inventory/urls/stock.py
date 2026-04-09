8ab7aa6068aae627d5e3d357e98d7a021618b205
ASCiiLex <alejandroscuetara@gmail.com>
2026-04-09 14:20:38 +0200

    Fix navegación HTMX, eliminar duplicidad de navbar y corregir flujo de transferencias

--- inventory/urls/stock.py
+++ inventory/urls/stock.py
@@ -5,11 +5,9 @@ from ..views.stock import (
     stockmovement_create,
     export_stockmovements_csv,
 )
-from ..views.transfers import transfer_create
 
 urlpatterns = [
     path("stock-movements/", stockmovement_list, name="stockmovement_list"),
     path("stock-movements/nuevo/", stockmovement_create, name="stockmovement_create"),
     path("stock-movements/exportar/", export_stockmovements_csv, name="export_stockmovements_csv"),
-    path("stock-movements/transferencia/", transfer_create),
 ]
